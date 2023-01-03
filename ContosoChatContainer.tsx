0a5eb2a305dca282403ec597cb369cea003ef479
Alex Kwan <alkwa@microsoft.com>
2023-01-03 08:46:33 -0800

    adding code to use communication-react and chat with nextjs

---
+++ ContosoChatContainer.tsx
@@ -0,0 +1,68 @@
+import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
+import {
+  ChatComposite,
+  CompositeLocale,
+  fromFlatCommunicationIdentifier,
+  useAzureCommunicationChatAdapter
+} from '@azure/communication-react';
+import { PartialTheme, Theme } from '@fluentui/react';
+import React, { useMemo } from 'react';
+
+export type ContainerProps = {
+  /** UserIdentifier is of type CommunicationUserIdentifier see below how to construct it from a string input */
+  userIdentifier: string;
+  token: string;
+  displayName: string;
+  endpointUrl: string;
+  threadId: string;
+  fluentTheme?: PartialTheme | Theme;
+  errorBar?: boolean;
+  participants?: boolean;
+  topic?: boolean;
+  locale?: CompositeLocale;
+  formFactor?: 'desktop' | 'mobile';
+};
+
+export const ContosoChatContainer = (props: ContainerProps): JSX.Element => {
+  const credential = useMemo(() => {
+    try {
+      return new AzureCommunicationTokenCredential(props.token);
+    } catch {
+      console.error('Failed to construct token credential');
+      return undefined;
+    }
+  }, [props.token]);
+
+  const userId = useMemo(
+    () => fromFlatCommunicationIdentifier(props.userIdentifier) as CommunicationUserIdentifier,
+    [props.userIdentifier]
+  );
+
+  const adapter = useAzureCommunicationChatAdapter({
+    endpoint: props.endpointUrl,
+    userId,
+    displayName: props.displayName,
+    credential,
+    threadId: props.threadId
+  });
+
+  if (adapter) {
+    return (
+      <div style={{ height: '90vh', width: '90vw' }}>
+        <ChatComposite
+          adapter={adapter}
+          fluentTheme={props.fluentTheme}
+          options={{
+            errorBar: props.errorBar,
+            topic: props.topic
+          }}
+          locale={props.locale}
+        />
+      </div>
+    );
+  }
+  if (credential === undefined) {
+    return <>Failed to construct credential. Provided token is malformed.</>;
+  }
+  return <>Initializing...</>;
+};
